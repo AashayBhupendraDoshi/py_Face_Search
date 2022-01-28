db5d44a8081d943bbc2aacc392ee4e3fd62cec17
Aashay Bhupendra Doshi <ch15b097@smail.iitm.ac.in>
2022-01-28 14:15:22 +0530

    Delete single_face.py

--- single_face.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from single_face_search.app_07 import Window
-
-
-app = QApplication(sys.argv)
-win = Window()
-win.show()
-sys.exit(app.exec_())
